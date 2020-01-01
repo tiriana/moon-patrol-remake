e64865bdb8c5159cfb787c79f644eb6a5c928fe9
Radomir Wojtera <rwojtera@egnyte.com>
2020-01-01 16:22:15 +0100

    Player death animation, destroying obstacles behind car. Background

---
+++ scenes/Destructable.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Destructable_area_entered(area):
+	print("KILLING PARENT");
+	get_parent().queue_free()
+	pass # Replace with function body.
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -9,24 +9,27 @@ const FLOOR = Vector2(0, -1)
 const ACCELERATION = 10
 const BREAKING = -20;
 
-var alive = true;
+onready var alive = true;
+onready var velocity = Vector2(0, 0)
+onready var is_jumping = false;
+onready var reached_min_speed = false
 
-var velocity = Vector2(0, 0)
-
-var is_jumping = false;
-
-var reached_min_speed = false
-
-func _ready():
-	pass 
+func _stick_to_the_ground():
+	var ground_y = get_node("GroundScanner").get_collision_point().y-75/2;
+	
+	if (not is_jumping || position.y >= ground_y):
+		position.y = ground_y;
+		is_jumping = false
 
 func _process(delta):
 	if !alive:
 		return
+	
 	if velocity.x < MIN_SPEED:
 		velocity.x += ACCELERATION 
 		velocity = move_and_slide(velocity, FLOOR);
-		return
+		_stick_to_the_ground();
+		return;
 	
 	if Input.is_action_pressed("ui_right") and not is_jumping:
 		velocity.x += ACCELERATION
@@ -34,7 +37,6 @@ func _process(delta):
 		velocity.x += BREAKING
 	elif not is_jumping:
 		velocity.x -= ACCELERATION;
-	
 	
 	if (not is_jumping):
 		velocity.x = clamp(velocity.x, MIN_SPEED, MAX_SPEED)
@@ -47,17 +49,14 @@ func _process(delta):
 			
 	velocity = move_and_slide(velocity, FLOOR);
 	
-	var ground_y = get_node("RayCast2D").get_collision_point().y-75/2;
-	
-	if (not is_jumping || position.y >= ground_y):
-		position.y = ground_y;
-		is_jumping = false
-	pass
-	
+	_stick_to_the_ground();
 	
 func die():
-	position += Vector2(-3000, -20);
-	get_node("Camera2D").shake(0.5,30,300);
+	alive = false;
+	velocity.x = 0;
+	_stick_to_the_ground();
+	start_blinking();
+	get_node("RespawnTimer").start()
 	
 const ENEMY_BULLET	= 16;
 const ENEMY  =8;
@@ -67,15 +66,28 @@ func _on_hit(area_body):
 	if (alive && [ROCK, ENEMY, ENEMY_BULLET].has(area_body.collision_layer)):
 		die();
 		area_body.queue_free();
-	
 
 func _on_Hitbox_area_entered(area):
 	_on_hit(area);
-		#is_jumping = true;
-		#velocity = move_and_slide(Vector2(-800, -300));
-	pass # Replace with function body.
-
 
 func _on_Hitbox_body_entered(body):
 	_on_hit(body);
+	
+func start_blinking():
+	get_node("BlinkingTimer").start();
+	visible = false;
+	
+func stop_blinking():
+	get_node("BlinkingTimer").stop();
+	visible = true;
+
+func _on_RespawnTimer_timeout():
+	position.x -= 1500;
+	stop_blinking();
+	_ready();
+	pass # Replace with function body.
+
+
+func _on_BlinkingTimer_timeout():
+	visible = !visible;
 	pass # Replace with function body.
